9ae7b6d196c2855f45ccdd841ec84dd96b8d2fa8
Jonah Wallerstein <jwallers@gmail.com>
2020-06-02 17:43:42 -0400

    Encapsulated in namespace

--- SimpleSvgImporter/Assets/Scripts/SvgElement.cs
+++ SimpleSvgImporter/Assets/Scripts/SvgElement.cs
@@ -1,24 +1,24 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Xml;
 using UnityEngine;
 
-
-[Serializable]
-public class SvgElement
+namespace SimpleSvgImport
 {
-    public bool HasFill;
+    [Serializable]
+    public class SvgElement
+    {
+        public bool HasFill;
 
-    public bool HasStroke;
+        public bool HasStroke;
 
-    public Color FillColor;
+        public Color FillColor;
 
-    public Color StrokeColor;
+        public Color StrokeColor;
 
-    public List<Vector2> Points = new List<Vector2>();
+        public List<Vector2> Points = new List<Vector2>();
 
-    public bool IsClosed;
-    
+        public bool IsClosed;
 
+
+    }
 }
--- SimpleSvgImporter/Assets/Scripts/SvgScriptable.cs
+++ SimpleSvgImporter/Assets/Scripts/SvgScriptable.cs
@@ -1,49 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System;
 using UnityEngine;
-using System.Xml;
 
-public class SvgScriptable : ScriptableObject
+namespace SimpleSvgImport
 {
-    public List<SvgElement> SvgData = new List<SvgElement>();
-
-
-    private void TransformPoints(Func<Vector2, Vector2> transFunc)
+    public class SvgScriptable : ScriptableObject
     {
-        foreach (var elem in SvgData)
-        {
-            for (int cntr = 0; cntr < elem.Points.Count; cntr++)
-            {
-                elem.Points[cntr] = transFunc(elem.Points[cntr]);
-            }
-        }
-    }
+        public List<SvgElement> SvgData = new List<SvgElement>();
 
-    public void Scale(float scaleVal)
-    {
-        TransformPoints(pt => pt * scaleVal);
-    }
 
-    public void Scale(Vector2 scaleVal)
-    {
-        TransformPoints(pt => pt * scaleVal);
-    }
-
-    public void Move(Vector2 posChange)
-    {
-        TransformPoints(pt => pt + posChange);
-    }
-
-    public IEnumerable<Vector2> AllPoints
-    {
-        get
+        private void TransformPoints(Func<Vector2, Vector2> transFunc)
         {
             foreach (var elem in SvgData)
             {
-                foreach (var point in elem.Points)
+                for (int cntr = 0; cntr < elem.Points.Count; cntr++)
                 {
-                    yield return point;
+                    elem.Points[cntr] = transFunc(elem.Points[cntr]);
+                }
+            }
+        }
+
+        public void Scale(float scaleVal)
+        {
+            TransformPoints(pt => pt * scaleVal);
+        }
+
+        public void Scale(Vector2 scaleVal)
+        {
+            TransformPoints(pt => pt * scaleVal);
+        }
+
+        public void Move(Vector2 posChange)
+        {
+            TransformPoints(pt => pt + posChange);
+        }
+
+        public IEnumerable<Vector2> AllPoints
+        {
+            get
+            {
+                foreach (var elem in SvgData)
+                {
+                    foreach (var point in elem.Points)
+                    {
+                        yield return point;
+                    }
                 }
             }
         }
